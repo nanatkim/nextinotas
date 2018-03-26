2037921723ac3653d88c4af74f22301d5323415f
Natali Brito <nanatkim11@gmail.com>
2018-03-26 15:18:43 -0300

    fix

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controller;
-use AppB\Entity\User;
+use App\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,21 +9,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class UserController extends Controller
 {
     /**
-     * @Route("/user", name="user_index")
+     * @Route("/{facul}/user", name="user_index")
      */
-    public function viewUserAction()
+    public function viewUserAction(int $facul)
     {
         $user = $this->getDoctrine()
             ->getRepository('App:User')
             ->findAll();
 
-            $facul = $_GET['facul'];
             $faculdade = $this->getDoctrine()
                 ->getRepository('App:Faculdade')
                 ->find($facul);
 
-        return $this->render('/user/view.html.twig',[
-            'user' => $user,
+        return $this->render('/user/index.html.twig',[
+            'users' => $user,
             'faculdade' => $faculdade,
             'qtd' => count($user)
         ]);
