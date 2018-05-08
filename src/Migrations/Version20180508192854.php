aca9b3a1aa8a6d1e4b20e1985e9f2174e503b1d6
Natali Brito <nanatkim11@gmail.com>
2018-05-08 16:56:41 -0300

    sim

---
+++ src/Migrations/Version20180508192854.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20180508192854 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE curso');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE curso (id INT AUTO_INCREMENT NOT NULL, nome VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci, id_turma INT NOT NULL, UNIQUE INDEX UNIQ_CA3B40ECC5875896 (id_turma), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+    }
+}
